9a3580fd3c4f90e05f31da8f0c19e48fee388e1a
JoeyMaton <jmaton52@outlook.fr>
2024-02-13 18:29:13 +0100

    v0.3

--- modal.js
+++ modal.js
@@ -40,22 +40,49 @@ function closeModal() {
    const errorFirstName = document.getElementById("error-firstname");
    const errorLastName = document.getElementById("error-lastname");
  
-   validateWithMinimumCharacters(balisePrenom.value, errorFirstName, "Prénom pas bon");
-   validateWithMinimumCharacters(baliseNom.value, errorLastName, "Nom pas bon");
+   validateWithMinimumCharacters(balisePrenom.value, errorFirstName, "Le prénom doit contenir 2 caractères au minimum. ");
+   validateWithMinimumCharacters(baliseNom.value, errorLastName, "Le nom doit contenir 2 caractères au minimum. ");
+   console.log(balisePrenom.value)
+   console.log(baliseNom.value);
  
    let baliseEmail = document.getElementById("email");
+   const errorEmail = document.getElementById("error-email");
+   validateEmail(baliseEmail.value, errorEmail, "L'email n'est pas valide. ")
    console.log(baliseEmail.value);
+
+   let baliseConcours = document.getElementById("quantity");
+   const errorConcours = document.getElementById("error-concours")
+   validateConcours(baliseConcours.value, errorConcours, "Une valeur numérique doit être saisie. ")
+   console.log(baliseConcours.value);
+
+   let ListBtnRadio = document.querySelectorAll("input[type=radio]")
+   for (let i = 0; i < ListBtnRadio.length; i++) {
+      if (ListBtnRadio[i].checked) {
+          console.log(ListBtnRadio[i].value)
+      }
+   }
+   const errorBtnRadio = document.getElementById("error-btnRadio")
+   
+
+   let checkbox1 = document.getElementById("checkbox1")
+   console.log(checkbox1.checked);
+
+   let checkbox2 = document.getElementById("checkbox2")
+   console.log(checkbox2.checked);
+
+
  });
+
 
  
  function validateWithMinimumCharacters(value, errorElement, errorMessage) {
    if (value.length < 2) {
      errorElement.innerText = errorMessage;
      return true;
-   }
+   } 
    errorElement.innerText = "";
    return false;
- };
+ }
 
 /**
  * Cette fonction prend un email en paramètre et valide qu'il est au bon format
@@ -63,27 +90,37 @@ function closeModal() {
  * @param {string} baliseEmail 
  * @throws {Error} 
  */
-function validerEmail(baliseEmail) {
+function validateEmail(baliseEmail, errorElement, errorMessage) {
   let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z._-]+")
   if (!emailRegExp.test(baliseEmail)) {
-    throw new Error("L'Email n'est pas valide. ")
+    errorElement.innerText = errorMessage;
+    return true;
   } 
+    errorElement.innerText = "";
+    return false;
 }
 
-function gererFormulaire() {
+function validateConcours(baliseConcours, errorElement, errorMessage) {
+  let concoursRegExp = new RegExp("[0-9]+")
+  if (!concoursRegExp.test(baliseConcours)) {
+    errorElement.innerText = errorMessage;
+    return true;
+  } 
+    errorElement.innerText = "";
+    return false;
+}
+
+function validateBtnRadio (ListBtnRadio, errorElement, errorMessage) {
+
+}
+
+
+function manageForm() {
   try {
-    let balisePrenom = document.getElementById("first")
-    validerPrenom(balisePrenom)
   
-    let baliseNom = document.getElementById("last")
-    validerNom(baliseNom)
-  
-    let baliseEmail = document.getElementById("email")
-    validerEmail(baliseEmail)
-    afficherMessageErreur("")
 
   } catch (erreur) {
     // gérer l'erreur
-    afficherMessageErreur(erreur.message)
+  
   }
 }
