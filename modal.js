3b892e440fac5f9b059e1162d87ea0e9ce96d527
JoeyMaton <jmaton52@outlook.fr>
2024-03-05 18:38:08 +0100

    v1.0

--- modal.js
+++ modal.js
@@ -27,6 +27,9 @@ closeBtn.addEventListener("click", closeModal);
 // close modal event
 function closeModal() {
   modalbg.style.display = "none";
+  document.querySelector(".modal-body").style.display = "block";
+  document.querySelector(".success").style.display = "none";
+  location.reload()
 }
 
  // Displays the recovered data
@@ -36,40 +39,38 @@ function closeModal() {
    event.preventDefault();
  
    let firstName = document.getElementById("first");
-   const hasFirstNameError = document.getElementById("error-firstname");
-   validateWithMinimumCharacters(firstName.value, hasFirstNameError, "Le prénom doit contenir 2 caractères au minimum. ");
+   const firstNameError = document.getElementById("error-firstname");
+   const hasFirstNameError = validateWithMinimumCharacters(firstName.value, firstNameError, "Le prénom doit contenir 2 caractères au minimum. ");
    console.log(firstName.value);
 
    let lastName = document.getElementById("last");
-   const hasLastNameError = document.getElementById("error-lastname");
-   validateWithMinimumCharacters(lastName.value, hasLastNameError, "Le nom doit contenir 2 caractères au minimum. ");
+   const lastNameError = document.getElementById("error-lastname");
+   const hasLastNameError = validateWithMinimumCharacters(lastName.value, lastNameError, "Le nom doit contenir 2 caractères au minimum. ");
    console.log(lastName.value);
  
    let email = document.getElementById("email");
-   const hasEmailError = document.getElementById("error-email");
-   validateEmail(email.value, hasEmailError, "L'email n'est pas valide. ")
+   const emailError = document.getElementById("error-email");
+   const hasEmailError = validateEmail(email.value, emailError, "L'email n'est pas valide. ")
    console.log(email.value);
 
    let birthDate = document.getElementById("birthdate");
-   const hasBirthDateError = document.getElementById("error-birthdate");
-   validateBirthDate(birthDate, hasBirthDateError, "Veuillez entrer une date de naissance valide.")
+   const birthDateError = document.getElementById("error-birthdate");
+   const hasBirthDateError = validateBirthDate(birthDate, birthDateError, "Veuillez entrer une date de naissance valide.")
    console.log(birthDate.value)
 
    let concours = document.getElementById("quantity");
-   const hasConcoursError = document.getElementById("error-concours");
-   validateConcours(concours.value, hasConcoursError, "Une valeur numérique compris entre 0 et 100 doit être saisie. ");
+   const concoursError = document.getElementById("error-concours");
+   const hasConcoursError = validateConcours(concours.value, concoursError, "Une valeur numérique compris entre 0 et 100 doit être saisie. ");
    console.log(concours.value);
 
    let listBtnRadio = document.querySelector("input[name='location']:checked");
-   const hasBtnRadioError = document.getElementById("error-btnRadio");
-   validateBtnRadio(listBtnRadio, hasBtnRadioError, "Un bouton doit être selectionné. ");
+   const btnRadioError = document.getElementById("error-btnRadio");
+   const hasBtnRadioError = validateBtnRadio(listBtnRadio, btnRadioError, "Un bouton doit être selectionné. ");
    console.log(listBtnRadio.value)
-  
-   
 
    let checkbox1 = document.getElementById("checkbox1");
-   const hasCheckboxError = document.getElementById("error-checkbox");
-   validateCheckbox(checkbox1, hasCheckboxError, "Vous devez accepter les conditions d'utilisation. ");
+   const checkboxError = document.getElementById("error-checkbox");
+   const hasCheckboxError = validateCheckbox(checkbox1, checkboxError, "Vous devez accepter les conditions d'utilisation. ");
    console.log(checkbox1.checked);
 
 // All return false
@@ -83,8 +84,8 @@ if (
   !hasCheckboxError
 )
   {
-    const modalContent = document.querySelector(".modal-body");
-    modalContent.innerHTML = `<h1>Merci ! Votre réservation a été reçue.</h1>`;
+    document.querySelector(".modal-body").style.display = "none";
+    document.querySelector(".success").style.display = "block";
   }
  });
 
